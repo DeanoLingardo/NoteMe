39926ac9a87425afbda4cca329d98448467bd2e0
Dean Lingard <deanlingard@yahoo.co.uk>
2018-01-03 13:16:50 +0000

    Setup

---
+++ app/src/main/java/com/example/daisygarcia/noteme/NotepadActivity.java
@@ -0,0 +1,13 @@
+package com.example.daisygarcia.noteme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotepadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad);
+    }
+}
